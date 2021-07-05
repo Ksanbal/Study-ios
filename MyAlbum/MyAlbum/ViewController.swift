a5ee2d2ec2baa2351ac083e2dfb8259afe73f9d6
K_Sanbal <dev.ksanbal@gmail.com>
2021-07-05 22:27:00 +0900

    init MyAlbum project

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert(_ sender: Any) {
+        // 팝업될 내용 설정
+        let alert = UIAlertController(
+            title:"Hello",
+            message: "My First App!!",
+            preferredStyle: .alert
+        )
+        // 팝업에 추가될 버튼 기능 설정
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+        )
+        // 버튼 기능을 팝업에 추가
+        alert.addAction(action)
 
+        // 팝업을 띄우는 코드
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
