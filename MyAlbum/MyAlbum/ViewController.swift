254eb134a390ef8a3f4eb995486ba712582616c1
K_Sanbal <dev.ksanbal@gmail.com>
2021-07-05 21:35:31 +0900

    init 1st Study - MyAlbum

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 김현균 on 2021/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
