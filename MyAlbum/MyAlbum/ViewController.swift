6df456b40889ce6ee8b968ac0eb3832a78dcb5cc
K_Sanbal <dev.ksanbal@gmail.com>
2021-07-06 13:37:58 +0900

    Done MyAlbum App

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -8,31 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var cureentValue: Int = 0
+    
+    @IBOutlet weak var priceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refresh()
     }
 
+
     @IBAction func showAlert(_ sender: Any) {
+        let message = "가격은 ₩\(cureentValue) 입니다."
+        
         // 팝업될 내용 설정
         let alert = UIAlertController(
             title:"Hello",
-            message: "My First App!!",
+            message: message,
             preferredStyle: .alert
         )
         // 팝업에 추가될 버튼 기능 설정
         let action = UIAlertAction(
             title: "OK",
             style: .default,
-            handler: nil
+            handler: {
+                action in self.refresh()
+            }
         )
         // 버튼 기능을 팝업에 추가
         alert.addAction(action)
-
         // 팝업을 띄우는 코드
-        present(alert, animated: true, completion: nil)           
+        present(alert, animated: true, completion: nil)
     }
     
+    func refresh(){
+        let randomPrice = arc4random_uniform(10000) + 1
+        cureentValue = Int(randomPrice)
+        priceLabel.text = "₩ \(cureentValue)"
+    }
 }
 
