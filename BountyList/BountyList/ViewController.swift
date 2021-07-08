c7422f774e0ade8035ced6c1f201ae9612ea6e65
K_Sanbal <dev.ksanbal@gmail.com>
2021-07-08 12:57:16 +0900

    init BountyList Project

---
+++ BountyList/BountyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by 김현균 on 2021/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
