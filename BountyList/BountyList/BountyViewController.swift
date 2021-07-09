0d28e4a2853a9715f9b71d056bdd4da59cf4aa66
K_Sanbal <dev.ksanbal@gmail.com>
2021-07-09 11:22:40 +0900

    Done BountyList with Add Animation

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BountyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     // MVVM
     
@@ -43,34 +43,49 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         super.viewDidLoad()
     }
     
-    // UITableViewDataSource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // UICollectionViewDataSource
+    // 몇개를 보여줄까요?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numOfBountyInfoList
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
-            return UITableViewCell()
+    // 셀은 어떻게 표현할까요?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
+            return UICollectionViewCell()
         }
-        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
         cell.update(info: bountyInfo)
         return cell
     }
     
-    // UITableViewDelegate
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("--> \(indexPath.row)")
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+    // UICollectionViewDelegate
+    // 셀이 클릭되었을때 어떻게 할까요?
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
+    }
+    
+    // UICollectionViewDelegateFlowLayout
+    // cell size를 계산할거다 (목표: 다양한 디바이스에서 일괁거인 디자인을 보여주기위해)
+    // 좌우 위아래 Spacing을 10이라고 계산
+    // Image는 7:10을 유지하도록, 아래 빈칸은 높이 65정도
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 10
+        let textAreaHeight: CGFloat = 65
+        
+        let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2
+        let height: CGFloat = width * 10/7 + textAreaHeight
+        return CGSize(width: width, height: height)
     }
 }
 
-class ListCell: UITableViewCell {
+class GridCell: UICollectionViewCell {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
     
     func update(info: BountyInfo) {
-        imageView?.image = info.image
+        imgView?.image = info.image
         nameLabel.text = info.name
         bountyLabel.text = String(info.bounty)
     }
@@ -81,7 +96,7 @@ class BountyViewModel {
         BountyInfo(name: "brook", bounty: 33000000),
         BountyInfo(name: "chopper", bounty: 50),
         BountyInfo(name: "franky", bounty: 44000000),
-        BountyInfo(name: "luffy", bounty: 30000000),
+        BountyInfo(name: "luffy", bounty: 300000000),
         BountyInfo(name: "nami", bounty: 16000000),
         BountyInfo(name: "robin", bounty: 80000000),
         BountyInfo(name: "sanji", bounty: 77000000),
@@ -103,3 +118,4 @@ class BountyViewModel {
         return sortedList[Index]
     }
 }
+
--- BountyList/BountyList/DetailViewController.swift
+++ BountyList/BountyList/DetailViewController.swift
@@ -25,12 +25,61 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLableCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
 
+    // View가 메모리에 올라가고 나서
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    
+    // View가 그려지고 나서
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+    }
+    
+    private func prepareAnimation() {
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
+    }
+    
+    private func showAnimation() {
+        UIView.animate(
+            withDuration: 1,
+            delay: 0.0,
+            usingSpringWithDamping: 0.6,    // 통통튀는 애니메이션
+            initialSpringVelocity: 2.0,
+            options: .allowUserInteraction,
+            animations: {
+                // 변형되기전의 모습
+                self.nameLabel.transform = CGAffineTransform.identity
+                self.nameLabel.alpha = 1
+            },
+            completion: nil
+        )
+        
+        UIView.animate(
+            withDuration: 1,
+            delay: 0.2,
+            usingSpringWithDamping: 0.6,    // 통통튀는 애니메이션
+            initialSpringVelocity: 2.0,
+            options: .allowUserInteraction,
+            animations: {
+                self.bountyLabel.transform = CGAffineTransform.identity
+                self.bountyLabel.alpha = 1
+            },
+            completion: nil
+        )
+        
+        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
     
     func updateUI() {
